dd6eadb00886899e1dc6ec7f175bc919fe0235d3
Dennis van der Bij <d.vanderbij@gmail.com>
2018-02-28 22:24:55 +0100

    Add broadcast module

--- src/main/scala/Broadcast.scala
+++ src/main/scala/Broadcast.scala
@@ -1,6 +1,14 @@
-object Network {
-  trait Alg[F[_], Data] {
-    def send(d: Data): F[Unit]
-    def receive: F[Data]
-  }
+
+
+trait Broadcast[F[_], Request, Response] {
+  def broadcast(request: Request): F[Unit]
+  def deliver: F[Request]
 }
+
+object Broadcast extends BroadcastInstances {
+  def apply[F[_], Request, Response](
+      implicit B: Broadcast[F, Request, Response])
+    : Broadcast[F, Request, Response] = B
+}
+
+sealed abstract class BroadcastInstances {}
