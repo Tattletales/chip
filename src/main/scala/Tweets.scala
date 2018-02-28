7f885ec2732175d492d1d188f466974ad480ea47
Dennis van der Bij <d.vanderbij@gmail.com>
2018-02-28 22:26:25 +0100

    Make tweets and users modules more user friendly

--- src/main/scala/Tweets.scala
+++ src/main/scala/Tweets.scala
@@ -1,9 +1,11 @@
-object Tweets {
-  trait Alg[F[_], User, Tweet] {
-    def getTweets(user: User): F[List[Tweet]]
-    def addTweet(user: User, tweet: Tweet): F[Unit]
-  }
+trait Tweets[F[_], User, Tweet] {
+  def getTweets(user: User): F[List[Tweet]]
+  def addTweet(user: User, tweet: Tweet): F[Unit]
 }
 
+object Tweets extends TweetsInstances {
+  def apply[F[_], User, Tweet](
+      implicit T: Tweets[F, User, Tweet]): Tweets[F, User, Tweet] = T
+}
 
-
+sealed abstract class TweetsInstances {}
--- src/main/scala/Users.scala
+++ src/main/scala/Users.scala
@@ -1,7 +1,12 @@
-object Users {
-  trait Alg[F[_], User, Signup] {
-    def addUser(signup: Signup): F[Unit] // Option?
-    def removeUser(user: User): F[Unit]
-    def searchUser(name: String): F[List[User]]
-  }
+trait Users[F[_], User, Signup] {
+  def addUser(signup: Signup): F[Unit] // Option?
+  def removeUser(user: User): F[Unit]
+  def searchUser(name: String): F[List[User]]
 }
+
+object Users extends UsersInstances {
+  def apply[F[_], User, Signup](
+      implicit U: Users[F, User, Signup]): Users[F, User, Signup] = U
+}
+
+sealed abstract class UsersInstances {}
