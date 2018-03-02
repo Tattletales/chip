840a48a44255fc6398abdc23d9ef1d6a7ef5b486
Dennis van der Bij <d.vanderbij@gmail.com>
2018-03-02 12:18:42 +0100

    Implement add user

--- src/main/scala/Users.scala
+++ src/main/scala/Users.scala
@@ -1,3 +1,7 @@
+import cats.Monad
+import cats.data.OptionT
+import cats.implicits._
+
 trait Users[F[_], User] {
   def addUser(name: String, password: String): F[Option[User]]
   def removeUser(user: User): F[Unit]
@@ -9,10 +13,22 @@ object Users extends UsersInstances {
 }
 
 sealed abstract class UsersInstances {
-  implicit def syncedUsers[F[_], User](
-      db: Database[F, String]): Users[F, User] = new Users[F, User] {
-    def addUser(name: String, password: String): F[User] = ???
-    
+  implicit def syncedUsers[F[_]: Monad, User, ](
+      db: Database[F, String],
+      client: HttpClient[F, String]): Users[F, User] = new Users[F, User] {
+    def addUser(name: String, password: String): F[Option[User]] =
+      (for {
+        user <- OptionT(
+          db.insertAndGet[User](
+            s"""
+           |INSERT INTO users (name, password)
+           |VALUES ($name, $password)
+       """.stripMargin,
+            Seq("id", "user", "password"): _*))
+
+        _ <- client.post(???, ???)
+      } yield user).value
+
     def removeUser(user: User): F[Unit] = ???
 
     def searchUser(name: String): F[List[User]] = db.query[User](???)
