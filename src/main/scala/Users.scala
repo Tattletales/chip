17974585f1fd78e4551375044d3e28bdf5f0c384
Dennis van der Bij <d.vanderbij@gmail.com>
2018-03-02 10:46:58 +0100

    synced users stub

--- src/main/scala/Users.scala
+++ src/main/scala/Users.scala
@@ -1,12 +1,20 @@
-trait Users[F[_], User, Signup] {
-  def addUser(signup: Signup): F[Unit] // Option?
+trait Users[F[_], User] {
+  def addUser(name: String, password: String): F[Option[User]]
   def removeUser(user: User): F[Unit]
   def searchUser(name: String): F[List[User]]
 }
 
 object Users extends UsersInstances {
-  def apply[F[_], User, Signup](
-      implicit U: Users[F, User, Signup]): Users[F, User, Signup] = U
+  def apply[F[_], User](implicit U: Users[F, User]): Users[F, User] = U
 }
 
-sealed abstract class UsersInstances {}
+sealed abstract class UsersInstances {
+  implicit def syncedUsers[F[_], User](
+      db: Database[F, String]): Users[F, User] = new Users[F, User] {
+    def addUser(name: String, password: String): F[User] = ???
+    
+    def removeUser(user: User): F[Unit] = ???
+
+    def searchUser(name: String): F[List[User]] = db.query[User](???)
+  }
+}
