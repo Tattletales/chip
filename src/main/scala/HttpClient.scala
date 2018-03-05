1e36f06a461560ebf69618dbb6dc0156aa855128
Florian Alonso <florian.alonso@epfl.ch>
2018-03-05 15:00:42 +0100

    Fixed compiler errors for POST requests

--- src/main/scala/HttpClient.scala
+++ src/main/scala/HttpClient.scala
@@ -1,27 +1,23 @@
+import cats.Monad
 import cats.effect._
-import cats.implicits._
 import fs2.Stream
 import org.http4s._
-import org.http4s.dsl._
-import org.http4s.client._
-import org.http4s.client.blaze.{BlazeClientConfig, Http1Client}
-import org.http4s.client.dsl.Http4sClientDsl
-import org.http4s._
 import org.http4s.client.blaze.Http1Client
-import org.http4s.client.dsl.Http4sClientDsl
-import org.http4s.dsl.io._
 
 trait HttpClient[F[_], G[_], Request] {
   def get[Response: EntityDecoder[G, ?]](request: Request): F[Response]
-  def post[T: EntityEncoder[G, ?]](request: Request, put: T): F[Unit]
+  def post[T, Response: EntityDecoder[G, ?]](request: Request, put: T)(
+    implicit T: EntityEncoder[G, T]
+  ): F[Response]
 }
 
 object HttpClient extends HttpClientInstances {
-  def apply[F[_], G[_], Request](implicit H: HttpClient[F, G, Request]): HttpClient[F, G, Request] = H
+  def apply[F[_], G[_], Request](implicit H: HttpClient[F, G, Request]): HttpClient[F, G, Request] =
+    H
 }
 
 sealed abstract class HttpClientInstances {
-  implicit def http4sClient[F[_]: Effect]: HttpClient[Stream[F, ?], F, String] =
+  implicit def http4sClient[F[_]: Monad: Effect]: HttpClient[Stream[F, ?], F, String] =
     new HttpClient[Stream[F, ?], F, String] {
       private[this] val safeClient = Http1Client.stream[F]()
 
@@ -31,22 +27,27 @@ sealed abstract class HttpClientInstances {
           out <- Stream.eval(client.expect[Response](request))
         } yield out
 
-      def post[T: EntityEncoder[F, ?]](request: String, put: T): Stream[F, Unit] = {
+      def post[T, Response: EntityDecoder[F, ?]](request: String, put: T)(
+        implicit w: EntityEncoder[F, T]
+      ): Stream[F, Response] = {
 
-        Uri.fromString(request) match {
+        val req = Uri.fromString(request) match {
           case Right(uri) =>
-            val req = Request()
-              .withMethod(Method.POST)
-              .withUri(uri)
-              .withBody(put)
+            Stream.emit(
+              Request()
+                .withMethod(Method.POST)
+                .withUri(uri)
+                .withBody(put)(implicitly[Monad[F]], w)
+            )
 
-            for {
-              client <- safeClient
-              out <- Stream.eval(client.expect[String](req))
-            } yield out
-
-          case Left(err) => Stream.raiseError[Unit](throw err)
+          case Left(err) => Stream.raiseError[F[Request[F]]](throw err)
         }
+
+        for {
+          client <- safeClient
+          req <- req.covary[F]
+          out <- Stream.eval(client.expect[Response](req))
+        } yield out
       }
     }
 }
