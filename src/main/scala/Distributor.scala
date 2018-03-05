52fd0abfc4e4638b3c076f85a6f2e0d3e0baaea4
Dennis van der Bij <d.vanderbij@gmail.com>
2018-03-05 19:03:29 +0100

    Distributor encodes data in json

--- src/main/scala/Distributor.scala
+++ src/main/scala/Distributor.scala
@@ -1,20 +1,26 @@
 import HttpClient.Uri
-import org.http4s.EntityEncoder
+import cats.Applicative
+import io.circe.Encoder
+import io.circe.syntax._
+import org.http4s.circe._
+import org.http4s.dsl.io._
 
 trait Distributor[F[_], G[_], Message] {
   def share(m: Message): F[Unit]
 }
 
 object Distributor extends DistributorInstances {
-  implicit def apply[F[_], G[_], Message](implicit D: Distributor[F, G, Message]): Distributor[F, G, Message] =
+  implicit def apply[F[_], G[_], Message](
+    implicit D: Distributor[F, G, Message]
+  ): Distributor[F, G, Message] =
     D
 }
 
 sealed abstract class DistributorInstances {
-  implicit def gossip[F[_], G[_], Message: EntityEncoder[G, ?]](
+  implicit def gossip[F[_], G[_]: Applicative, Message: Encoder](
     uri: Uri,
     httpClient: HttpClient[F, G]
   ): Distributor[F, G, Message] = new Distributor[F, G, Message] {
-    def share(m: Message): F[Unit] = httpClient.postAndIgnore(uri, m)
+    def share(m: Message): F[Unit] = httpClient.postAndIgnore(uri, m.asJson)
   }
 }
