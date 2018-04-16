a7aad007127cf7ffc222e567d70f61d9252adad5
Dennis van der Bij <d.vanderbij@gmail.com>
2018-04-16 13:14:39 +0200

    Add db backed KVStore and put_* for all-or-nothing puts

--- app/jvm/src/main/scala/backend/storage/KVStore.scala
+++ app/jvm/src/main/scala/backend/storage/KVStore.scala
@@ -1,22 +1,50 @@
 package backend.storage
 
+import cats.Functor
 import cats.data.State
+import cats.implicits._
+import doobie.implicits._
+import doobie.util.composite.Composite
+import doobie.util.fragment.Fragment
+import doobie.util.meta.Meta
 
 trait KVStore[F[_], K, V] {
   def get(k: K): F[Option[V]]
   def put(k: K, v: V): F[Unit]
+  def put_*(kv: (K, V), kvs: (K, V)*): F[Unit]
   def remove(k: K): F[Unit]
 }
 
-object KVStore extends KVStoreInstances {
-  def apply[F[_], K, V](implicit K: KVStore[F, K, V]): KVStore[F, K, V] = K
-}
-
-sealed abstract class KVStoreInstances {
-  implicit def stateKVS[K, V]: KVStore[State[Map[K, V], ?], K, V] =
+object KVStore {
+  def stateKVS[K, V]: KVStore[State[Map[K, V], ?], K, V] =
     new KVStore[State[Map[K, V], ?], K, V] {
       def get(k: K): State[Map[K, V], Option[V]] = State.inspect(_.get(k))
       def put(k: K, v: V): State[Map[K, V], Unit] = State.modify(_ + (k -> v))
+      def put_*(kv: (K, V), kvs: (K, V)*) = kvs.foldLeft(put(kv._1, kv._2)) {
+        case (state, (k, v)) => state.modify(_ + (k -> v))
+      }
       def remove(k: K): State[Map[K, V], Unit] = State.modify(_ - k)
+    }
+
+  def dbKVS[F[_]: Functor, K: Meta, V: Composite: Meta](db: Database[F]): KVStore[F, K, V] =
+    new KVStore[F, K, V] {
+      def get(k: K): F[Option[V]] = db.query(sql"""
+           SELECT value
+           FROM kv_store
+           WHERE $k = key
+         """.stripMargin).map(_.headOption)
+
+      def put(k: K, v: V): F[Unit] = put_*((k, v))
+
+      def put_*(x: (K, V), xs: (K, V)*): F[Unit] =
+        db.insert(update(x._1, x._2), xs.map { case (k, v) => update(k, v) }: _*)
+
+      def remove(k: K): F[Unit] = ???
+
+      private def update(k: K, v: V): Fragment = sql"""
+        UPDATE kv_store
+        SET value = $v
+        WHERE key = $k
+      """.stripMargin
     }
 }
