721ea84ac5592cdd3872ff146c91ad7c0014c316
Dennis van der Bij <d.vanderbij@gmail.com>
2018-06-05 10:23:54 +0200

    Fix issue where Money could not be 0

--- vault/src/main/scala/model/package.scala
+++ vault/src/main/scala/model/package.scala
@@ -6,13 +6,16 @@ import backend.storage.KVStore
 import cats.data.NonEmptyList
 import cats.effect.Effect
 import eu.timepit.refined.api.Refined
-import eu.timepit.refined.numeric.Positive
+import eu.timepit.refined.numeric.NonNegative
 import vault.events.TransactionStage
 
 package object model {
   type User = NodeId
-  type Money = Double Refined Positive
+  type Money = Double Refined NonNegative
 
+  /**
+    * Create a an [[Accounts]] with the accounts of the provided users.
+    */
   def accounts[F[_]: Effect, E: Gossipable](us: NonEmptyList[User])(
       daemon: GossipDaemon[F, TransactionStage, E],
       kvs: KVStore[F, User, Money]): F[Accounts[F]] =
